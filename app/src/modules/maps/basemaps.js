5684d40af027184843a851cf5c61ec7d2ec8e954
portal-civic <paumenjapa@gmail.com>
2026-03-10 23:37:57 +0100

    Reorganitza els mapes base en un catàleg estable

--- app/src/modules/maps/basemaps.js
+++ app/src/modules/maps/basemaps.js
@@ -1,4 +1,4 @@
-export const basemapOptions = [
+export const basemapCatalog = [
   {
     id: 'osm',
     label: 'OpenStreetMap',
@@ -12,7 +12,6 @@ export const basemapOptions = [
     url: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    subdomains: 'abcd',
     maxZoom: 20,
   },
   {
@@ -21,9 +20,10 @@ export const basemapOptions = [
     url: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    subdomains: 'abcd',
     maxZoom: 20,
   },
 ]
+
+export const basemapOptions = basemapCatalog
 
 export const defaultBasemapId = 'osm'
